7c83f67216cc44a28a701a6313b63d76bd9c1c1a
priyanshumahey <72003945+priyanshumahey@users.noreply.github.com>
2022-06-04 11:11:07 -0700

    Added button

--- game/src/App.js
+++ game/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>{props.text}</button>
+  )
+}
+
+
 function App() {
   return (
     <div>
     <h1>Color Game</h1>
+
       <div className="App">
+      <Button text="Button that says hello" onClick={alert('Hello')}/> 
         <div className='parent'>
           <div className='child' border="10px"></div>
           <div className='child'></div>

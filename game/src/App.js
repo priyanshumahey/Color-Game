88b86cb50c4db29e504371ba036dae946626e198
priyanshumahey <72003945+priyanshumahey@users.noreply.github.com>
2022-06-05 14:07:09 -0700

    Updates

--- game/src/App.js
+++ game/src/App.js
@@ -8,7 +8,20 @@ const Button = (props) => {
   )
 }
 
-function Head () {
+
+function Head (props) {
+  const time = 10
+  return(
+  <div>
+    <h1>Color Game</h1>
+    <p>Welcome to the Color Game! This is a simple game where the objective is to pick the different color. Start off by picking Easy, Medium or Hard.
+      Then the game will begin and you will have {time} seconds to pick the right color.
+    </p>
+  </div>
+  )
+}
+
+function Main () {
   const [mode, setMode] = React.useState('Choose Mode:')
   const [easyisShown, seteasyisShown] = React.useState(false)
   const [medisShown, setmedisShown] = React.useState(false)
@@ -33,12 +46,11 @@ function Head () {
   }
   return(
   <div>
-    <h1>Color Game</h1>
     <h2 className="mode">{mode}</h2>
     <div className="App">
-      <Button text="Easy" onClick={easyClick}/>
-      <Button text="Medium" onClick={medClick}/>
-      <Button text="Hard" onClick={hardClick}/>
+      {!easyisShown && !medisShown && !hardisShown && <Button text="Easy" onClick={easyClick}/>}
+      {!easyisShown && !medisShown && !hardisShown && <Button text="Medium" onClick={medClick}/>}
+      {!easyisShown && !medisShown && !hardisShown && <Button text="Hard" onClick={hardClick}/>}
     </div>
     {easyisShown && <Easy />}
     {medisShown && <Med />}
@@ -121,11 +133,11 @@ function Hard () {
 
 function Footer () {
   return (
-    <div>
+    <footer>
       <h3>Made by Priyanshu Mahey</h3>
       <h3><a href="https://www.linkedin.com/in/priyanshu-mahey/" rel="noreferrer" target="_blank">LinkedIn</a></h3>
       <h3><a href="https://github.com/priyanshumahey" rel="noreferrer" target="_blank">Github</a></h3>
-    </div>
+    </footer>
   )
 }
 
@@ -134,6 +146,7 @@ function App () {
   return (
     <div>
       <Head />
+      <Main />
       <Footer />
     </div>
   )

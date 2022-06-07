34594364f57b9b50b5f819aaa88e7ab4566501a7
priyanshumahey <72003945+priyanshumahey@users.noreply.github.com>
2022-06-07 10:57:15 -0700

    Update App.js

--- game/src/App.js
+++ game/src/App.js
@@ -20,6 +20,25 @@ const Timer = () => {
   )
 }
 
+function Example() {
+  const [count, setCount] = React.useState(0);
+
+  // Similar to componentDidMount and componentDidUpdate:
+  React.useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+  });
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
+}
+
 function shuffle(array) {
   let cI = array.length,  rI
   while (cI !== 0) {
@@ -107,6 +126,7 @@ function Main () {
     <div className="App">
       {(easyisShown || medisShown || hardisShown) &&<Button text="Change Difficulty" onClick={clear}/>}
     </div>
+    <Example />
      
     {easyisShown && <div><Timer /><Easy /></div>}
     {medisShown && <div><Timer /><Med /></div>}
